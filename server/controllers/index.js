c7382f62e8349ab44f046c3da735767226c7e8b5
Benjamin Kingsley (RIT Student) <bdk8938@rit.edu>
2026-03-16 12:50:52 -0400

    template

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,3 +1,5 @@
+const movies = require('../../data/movies.json');
+
 const hostIndex = (req, res) => {
   res.render('index');
 };
@@ -8,7 +10,35 @@ const notFound = (req, res) => {
   });
 };
 
+const getData = (req, res) => {
+  res.json(movies.filter(m => m.title === 'Taken'));
+}
+
+const results = (req, res) => {
+  let results = movies;
+  if (req.query.title) {
+    const lowerTitle = req.query.title.toLowerCase();
+    results = results.filter(m => m.title.toLowerCase().includes(lowerTitle));
+  }
+
+  if (req.query.year) {
+    const year = parseInt(req.query.year, 10);
+    results = results.filter(m => m.year === year);
+  }
+
+  if (req.query.starring) {
+    results = results.filter(m => m.cast.includes(req.query.starring));
+  }
+
+  res.render('results', {
+    search: req.query,
+    movies: results,
+  });
+}
+
 module.exports = {
   index: hostIndex,
   notFound,
+  getData,
+  results
 };
